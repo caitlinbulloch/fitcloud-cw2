c2bda8e8586ef744240970dbd56bdf806537039c
Caitlin Bulloch <bulloch-c@ulster.ac.uk>
2026-01-08 14:56:19 +0000

    fixing CORS issue

--- JS/view.js
+++ JS/view.js
@@ -3,7 +3,7 @@
 // Logic App endpoints
 const GET_API_URL = "https://prod-63.uksouth.logic.azure.com:443/workflows/f76358b2114c409c9e2d117f2be3c587/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=sLYILTIDHgZqd5wLlV7aARLQCyWhE9qBzM0p5S-gI5Y";
 const UPDATE_API_URL = "https://prod-39.uksouth.logic.azure.com/workflows/bfa4c9f449fa439596756088518a140a/triggers/When_an_HTTP_request_is_received/paths/invoke/workouts/%7Bid%7D?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=jFgOG8CpnNYf4lHOe1DJul47oYgS8IkAQNJAhqPObBw";
-const SEARCH_URL = "https://fitcloud-search-cw2.search.windows.net/indexes/fitcloud-search-ai/docs/search?api-version=2024-07-01";
+const SEARCH_URL = "https://prod-52.uksouth.logic.azure.com:443/workflows/250b8af8c0f349838dee5dfe8e76aade/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=kq2NDoW2B9fWvJLasB0u5DXc4iKw_2cI1hDqViIBxxs";
 const SEARCH_KEY = localStorage.getItem("AZURE_SEARCH_KEY");
 
 $(document).ready(() => {
@@ -12,26 +12,32 @@ $(document).ready(() => {
   // Search filter
   $("#searchInput").on("input", async function () {
     const term = $(this).val().trim();
-    if (term === "") {
-        fetchWorkouts(); // fallback to all
+
+    if (term.length === 0) {
+        fetchWorkouts(); // show all again
         return;
     }
 
     try {
         const response = await fetch(SEARCH_URL, {
         method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "api-key": SEARCH_KEY
-        },
-        body: JSON.stringify({ search: term })
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ query: term })
         });
-        const result = await response.json();
-        renderWorkouts(result.value);
+
+        if (!response.ok) throw new Error(await response.text());
+
+        const results = await response.json();
+        console.log("🔍 Search results:", results);
+
+        // Azure Search returns docs in "value"
+        const workouts = results.value || [];
+        renderWorkouts(workouts);
     } catch (err) {
         console.error("Search error:", err);
     }
   });
+
 
 
   // Cancel modal
